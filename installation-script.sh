ad7b88a03bfb02e583ee5952940c39fad47f7bc3
staffnerluca <113279240+staffnerluca@users.noreply.github.com>
2023-11-19 12:46:55 +0100

    Create installation-script.sh

---
+++ installation-script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install awesome
+sudo apt install nvim
+sudo apt install vim
+sudo apt install texstudio
+sudo apt install texlive-full
+sudo apt install npm
+sudo apt install python
+sudo apt install gimp
+sudo apt install cowsay
+sudo apt install lolcat
+sudo apt install anki
+sudo apt install blender
+sudo apt install obs-studio
+sudo apt install kdenlive
+
+sudo snap install bitwarden
+sudo snap install code --classic
+sudo snap install discord
+sudo snap install signal-desktop
+sudo snap install brave
+sudo snap install obsidian --classic
+sudo snap install notion-snap-reborn
+
+npm instal node
+npm install vite
+npm install react
